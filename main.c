594157f9d22b8a8cf78647216f37140b773c3467
Kevin <walshkev@oregonstate.edu>
2023-09-29 18:02:48 -0700

    new project hello world

---
+++ main.c
@@ -0,0 +1,14 @@
+/**************************************** 
+ * C-ploration 0 for CS 271 
+ *   
+ * [NAME] kevin walsh
+ * [TERM] FALL 2023 
+ *****************************************/
+#include <stdio.h>
+
+int main() {
+   // printf(.) displays the string given
+   printf("Hello world!\n");
+   // main returns 0 if exiting without error
+   return 0;
+}
